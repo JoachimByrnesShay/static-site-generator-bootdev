2cf1b254b829c1c2378cc217f00c55fe760b3dac
Joachim Byrnes-Shay <56098202+JoachimByrnesShay@users.noreply.github.com>
2024-04-14 22:55:38 -0400

    add no_tag and no_value unittests to test_leafnode

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -6,6 +6,16 @@ class TestLeafNode(unittest.TestCase):
         node = LeafNode('a', "Click here!", {"href": "https://www.yahoo.com", "target": "_blank"})
         expected = '<a href="https://www.yahoo.com" target="_blank">Click here!</a>'
         return self.assertEqual(node.to_html(), expected)
+    
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Click here buddy!")
+        return self.assertEqual(node.to_html(), node.value)
+    
+    def test_to_html_no_value(self):
+        node = LeafNode("div", None)
+        with self.assertRaises(ValueError):
+             node.to_html()
 
+       
 if __name__ == "__main__":
     unittest.main()
